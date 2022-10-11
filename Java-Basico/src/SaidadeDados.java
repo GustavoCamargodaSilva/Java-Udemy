d4ab64058f94f6ea122640fe7b41e24b6dad38da
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-11 13:38:49 -0300

    Saída de dados básica em Java

---
+++ Java-Basico/src/SaidadeDados.java
@@ -0,0 +1,13 @@
+public class SaidadeDados {
+    public static void main(String[] args) throws Exception {
+
+        int y = 32; // declaracao de valor da variavel int
+        System.out.println(y); // chamando a impressão da variavel no console
+
+        double x = 10.35784; // declaracao de valor variavel double
+        System.out.println(x); // impressao normal do float
+        System.out.printf("%.2f%n", x); // %.2f utilizado para limitar as casa decimais na impressoa %n utilizado para
+                                        // quebra de linha , x passado como parâmetro.
+
+    }
+}
