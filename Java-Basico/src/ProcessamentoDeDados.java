0d26dbf06ad054fec2556d72e4d44013b1e144e8
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-11 14:14:41 -0300

    ProcessamentoDeDados e Casting(conversão de dados)

---
+++ Java-Basico/src/ProcessamentoDeDados.java
@@ -0,0 +1,38 @@
+public class ProcessamentoDeDados {
+    public static void main(String[] args) {
+
+        int x, y; // declaracao dos tipos da variavel
+
+        x = 5; // atribuição de valor
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        // calculo área de quadrado
+
+        // double b, B, h, area;
+
+        // b = 6.0;
+        // B = 8.0;
+        // h = 5.0;
+
+        // area = (b + B) / 2.0 * h;
+
+        // System.out.println(area);
+
+        // Calculo de inteiros com resultado double
+
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+
+        resultado = (double) a / b; // Casting declarando a conversão antes do
+        // resultado da equação para que seja entregue numero double.
+
+        System.out.println(resultado);
+
+    }
+}
