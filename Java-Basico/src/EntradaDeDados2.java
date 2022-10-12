55b4864d0050c5ade4e8ce9339ffcdf7e538d33f
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-12 09:58:00 -0300

    Entrada de dados next e quebra de linha

---
+++ Java-Basico/src/EntradaDeDados2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EntradaDeDados2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String s1, s2, s3;
+
+        s1 = scan.nextLine();
+        s2 = scan.nextLine();
+        s3 = scan.nextLine();
+
+        System.out.println("Dados Digitados:");
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+    }
+}
