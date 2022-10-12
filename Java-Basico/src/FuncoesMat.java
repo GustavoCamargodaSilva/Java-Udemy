c35ad4225896aec315410d4531151a18dd458d89
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-12 10:05:47 -0300

    função raiz quadrada

---
+++ Java-Basico/src/FuncoesMat.java
@@ -0,0 +1,10 @@
+public class FuncoesMat {
+    public static void main(String[] args) {
+        double x, y, z, A, B, C;
+
+        A = 10;
+        x = 2;
+
+        A = Math.sqrt(x); // Função raiz quadrada
+    }
+}
