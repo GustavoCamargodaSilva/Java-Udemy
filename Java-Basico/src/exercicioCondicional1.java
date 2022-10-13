2c781abe1c6c51a003cfb0306ba5963811a0a7aa
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-13 10:49:39 -0300

    Exercicio exercicioCondicional 1

---
+++ Java-Basico/src/exercicioCondicional1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class exercicioCondicional1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o numero: ");
+        int question = scan.nextInt();
+
+        if (question < 0) {
+            System.out.println("NEGATIVO");
+        } else {
+            System.out.println("NAO NEGATIVO");
+        }
+    }
+}
