c0a19a7a46fffb52e9d94b56f95c95f74e17243f
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-14 09:28:37 -0300

    estruturaSwitchCase

---
+++ Java-Basico/src/estruturaSwitchCase.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class estruturaSwitchCase {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int x = scan.nextInt();
+        String dia;
+
+        switch (x) {
+            case 1:
+                dia = "Domingo";
+                break;
+            case 2:
+                dia = "Segunda";
+                break;
+            case 3:
+                dia = "Terça";
+                break;
+            case 4:
+                dia = "Quarta";
+                break;
+            case 5:
+                dia = "Quinta";
+                break;
+            case 6:
+                dia = "Sexta";
+                break;
+            case 7:
+                dia = "Sábado";
+                break;
+            default:
+                dia = "Valor invalido";
+                break;
+        }
+        System.out.println(dia);
+    }
+}
