ba068509fb8ccd1d60156d9bf8c75370fd58084f
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-14 09:05:49 -0300

    exercicioCondicional5

--- Java-Basico/src/exercicioCondicional5.java
+++ Java-Basico/src/exercicioCondicional5.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Locale;
 
-public class exerciciosCondicionais5 {
+public class exercicioCondicional5 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Locale.setDefault(Locale.US);
