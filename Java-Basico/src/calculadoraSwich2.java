d164e2a7c0a4e19abd584217d16c6d1aa8163438
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-16 11:00:39 -0300

    calculadoraSwich2 com Dowhile e swich

---
+++ Java-Basico/src/calculadoraSwich2.java
@@ -0,0 +1,46 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class calculadoraSwich2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        double num1, num2, resultado;
+        resultado = 0;
+        int contador = 0;
+
+        do {
+
+            System.out.println("Digite a operação que deseja fazer (+) (-) (*) (/)");
+            String operador = scan.next();
+
+            System.out.println("Digite o número 1");
+            num1 = scan.nextDouble();
+
+            System.out.println("Digite o número 2");
+            num2 = scan.nextDouble();
+
+            switch (operador) {
+                case "+":
+                    resultado = num1 + num2;
+                    System.out.println(resultado);
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    System.out.println(resultado);
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    System.out.println(resultado);
+                    break;
+                case "/":
+                    resultado = num1 * num2;
+                    System.out.println(resultado);
+                    break;
+            }
+        } while (resultado > 0);
+        contador++;
+
+    }
+}
