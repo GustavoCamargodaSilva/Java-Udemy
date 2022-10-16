f1b7707d7c183099fb7d6f21597b18b0085cf8ae
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-16 20:33:10 -0300

    doWhile

---
+++ Java-Basico/src/doWhile.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+import javax.print.attribute.SupportedValuesAttribute;
+
+import java.util.Locale;
+
+public class doWhile {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        char resp;
+
+        do {
+            System.out.print("Digite a temperatura em Celsius");
+            double C = scan.nextDouble();
+
+            double F = 9.0 * C / 5.0 + 32.0;
+
+            System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+
+            System.out.println("Deseja repetir (s/n)? ");
+            resp = scan.next().charAt(0);
+
+        } while (resp != 'n');
+
+        scan.close();
+
+    }
+}
