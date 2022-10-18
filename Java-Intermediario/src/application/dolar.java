166c0ada0859c24086ce7e9313f0ad2630298d3f
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-18 09:04:43 -0300

    conversor de Dollar

---
+++ Java-Intermediario/src/application/dolar.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.converter;
+
+public class dolar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        converter converter = new converter();
+
+        System.out.print("Digite o preço do Dollar atual: ");
+        converter.getDollarValue = scan.nextDouble();
+
+        System.out.print("Quantos Dolares você deseja comprar? ");
+        converter.getDollarBuy = scan.nextDouble();
+
+        System.out.printf("O Preço do Dollar é: R$ %.2f ", converter.getDollarValue);
+        System.out.printf("Você quer comprar: $ %.2f ", converter.getDollarBuy);
+
+        System.out.printf("O Preço que você vai pagar em Real vai ser: $ %.2f ", converter.calc());
+
+    }
+}
---
+++ Java-Intermediario/src/entities/converter.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class converter {
+    public double getDollarValue;
+    public double getDollarBuy;
+
+    public double calc() {
+        double intentionDollar = getDollarValue * getDollarBuy;
+        return intentionDollar;
+    }
+
+}
