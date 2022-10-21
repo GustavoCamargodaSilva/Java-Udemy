510ee363544393f1142b3e59a935907d51efb402
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-21 09:32:42 -0300

    forEach

---
+++ Java-Intermediario/src/application/forEach.java
@@ -0,0 +1,12 @@
+package application;
+
+public class forEach {
+    public static void main(String[] args) {
+
+        String[] vetor = new String[] { "Maria", "Bob", "Alex" };
+
+        for (String obj : vetor) {
+            System.out.println(obj);
+        }
+    }
+}
