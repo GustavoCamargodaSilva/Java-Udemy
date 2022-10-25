0b28bb1782153c20a89eed261ed9159b171e5dd8
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-25 16:36:27 -0300

    ExercicioProduto

---
+++ Java-Avançado/ExercicioProduto/src/entities/orderItem.java
@@ -0,0 +1,62 @@
+package entities;
+
+public class orderItem {
+    private Integer quantity;
+    private Double price;
+
+    private product product;
+
+    public orderItem() {
+
+    }
+
+    public orderItem(Integer quantity, Double price, product product) {
+        this.quantity = quantity;
+        this.price = price;
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public product getProduct() {
+        return product;
+    }
+
+    public void setProduct(product product) {
+        this.product = product;
+    }
+
+    public Double subTotal() {
+        return price * quantity;
+    }
+
+    public String toString() {
+        return product.getName()
+                + ", $"
+                + String.format("%.2f", price)
+                + ", Quantity: "
+                + quantity +
+                ", Subtotal: $"
+                + String.format("%.2f", subTotal());
+    }
+
+    public void remove(orderItem itens) {
+    }
+
+    public void add(orderItem itens) {
+    }
+}
